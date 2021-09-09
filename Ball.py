6b8c9442908c5a3913f194e229f6d4fa8416c149
Arnav G <arnavg286@gamil.com>
2021-09-08 18:13:32 -0700

    many updates

--- Ball.py
+++ Ball.py
@@ -1,21 +1,27 @@
+import time
 from Constants import HEIGHT, RECT_X, RECT_Y, WIDTH
-import random
 import pygame
 import math
-
+from random import randint
 class Ball:
-    def __init__(self,length) -> None:
-        self.genes = self.generate_genes(length)
+    def __init__(self,length,fromother:bool= False, ball1= None,ball2= None) -> None:
+
+        if fromother:
+            self.genes = self.gen_from_parents(ball1,ball2)
+        else:
+            self.genes = self.generate_genes(length)
         self.x =15
         self.y =15
         self.increment = 0
         self.done = False
         self.fitness = 0
+        self.reached = False
+
     def generate_genes(self,length:int):
         rep = []
 
         for i in range(length):
-            rep.append([random.randint(-10,10), random.randint(-10,10)])
+            rep.append([randint(-30,30), randint(-30,30)])
         return rep
     
     def draw(self,canvas):
@@ -23,16 +29,16 @@ class Ball:
     
     def move(self):
         if self.increment < len(self.genes):
-            if (self.x + self.genes[self.increment][0]) > 15 and (self.x + self.genes[self.increment][0]) < (HEIGHT-15):
+            if (self.x + self.genes[self.increment][0]) > 15 and (self.x + self.genes[self.increment][0]) < (WIDTH-15):
                 self.x += self.genes[self.increment][0]
             else:
                 self.x += -1*self.genes[self.increment][0]
-            if (self.y + self.genes[self.increment][1]) > 15 and (self.y + self.genes[self.increment][1]) < (WIDTH-15):
+            if (self.y + self.genes[self.increment][1]) > 15 and (self.y + self.genes[self.increment][1]) < (HEIGHT-15):
                 self.y += (self.genes[self.increment][1])
             else:
                 self.y += (-1*self.genes[self.increment][1])
             self.calc_dist()
-
+            self.if_dist()
             self.increment +=1
         else:
             self.done = True
@@ -40,3 +46,26 @@ class Ball:
     def calc_dist(self):
         dist = math.sqrt(math.pow((RECT_X-self.x),2)+math.pow(RECT_Y-self.y,2))
         self.fitness = 1- (dist/math.sqrt(math.pow((RECT_X-0),2)+math.pow(RECT_Y-0,2)))
+    
+    def gen_from_parents(self,ball, ball1):
+        res = []
+        for i in range(len(ball.genes)):
+
+            if randint(1,6) == 1:
+                res.append([randint(-30,30), randint(-30,30)])
+            else:
+                if randint(1,2) == 1:
+                    res.append(ball.genes[i])
+                else:
+                    res.append(ball1.genes[i])    
+            
+        return res
+    
+    def if_dist(self):
+        if self.fitness > 0.95:
+            print("done")
+
+            exit()
+
+
+
--- main.py
+++ main.py
@@ -1,36 +1,71 @@
 
 
-import math
 from Constants import HEIGHT, RECT_X, RECT_Y, WIDTH
 from Ball import Ball
 import pygame
 import time
 
-NUM = int(input("Num of balls"))
+NUM = 0
+class gen:
+    def __init__(self) -> None:
+        self.gen = 1
+    def incre(self):
+        self.gen = self.gen +1
+class boolwrapper:
+    def __init__(self) -> None:
+        self.boolean = False
+
+BOOLWRAPPER = boolwrapper()
+
+while NUM <5:
+    NUM = int(input("How many balls do you want > "))
 
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 
 BALLARRAY = []
 pygame.font.init()
 FONT = pygame.font.SysFont('Comic Sans MS',30)
+GEN = gen()
 
 def init():
     for i in range(NUM):
-        BALLARRAY.append(Ball(50))
+        BALLARRAY.append(Ball(120))
+
+def from_gen(copyarray):
+    total = []
+    for ind, ball in enumerate(copyarray):
+        
+        total.append({"fitness":ball.fitness,"index":ind})
+    key_func = lambda x: x.get("fitness")
+    total.sort(reverse=True,key=key_func)
+    print(total[0:10])
+    for i in range(NUM):
+        BALLARRAY.append(Ball(120,fromother=True, ball1=copyarray[total[0]["index"]],ball2=copyarray[total[1]["index"]]))
 
 
 
 def update_display():
+    
     WIN.fill((255,255,255))
-    pygame.draw.rect(WIN,(0,0,0),pygame.Rect(RECT_X-20,RECT_Y-20,20,20))
-    WIN.blit(FONT.render(str(round(calc_avg(),2)),False,(0,0,0)),(800,0))
-    for i in BALLARRAY:
-        i.draw(WIN)
-        i.move()
-    if len(BALLARRAY)>0 and BALLARRAY[0].done:
-        BALLARRAY.clear()
-        time.sleep(1)
-        init()
+    if not BOOLWRAPPER.boolean:
+        pygame.draw.rect(WIN,(0,0,0),pygame.Rect(RECT_X-20,RECT_Y-20,20,20))
+        WIN.blit(FONT.render(f"Average Fitness {str(round(calc_avg(),2))}",False,(0,0,0)),(560,0))
+        WIN.blit(FONT.render(f"Generation {GEN.gen}",False,(0,0,0)),(300,0))
+        for i in BALLARRAY:
+            i.draw(WIN)
+            i.move()
+            
+        if len(BALLARRAY)>0 and BALLARRAY[0].done:
+            copy = BALLARRAY.copy()
+            BALLARRAY.clear()
+            from_gen(copy)
+            time.sleep(1)
+            GEN.incre()
+        
+        
+    else:
+        WIN.blit(FONT.render(f"DONE",False,(0,0,0)),(450,100))
+        
     
     
     pygame.display.update()
@@ -41,14 +76,14 @@ def main():
     run = True
     init()
     clock = pygame.time.Clock()
-
+    
     while run:
         clock.tick(10)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-
+        
         update_display()
         
 def calc_avg():
